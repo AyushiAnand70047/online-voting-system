717f1a8f32cf24cf553db62cd406b9b0ac7a91b5
AyushiAnand70047 <ayushianand088@gmail.com>
2024-09-21 11:51:44 +0530

    login form and registration form structure

---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+    <head>
+        <title>Online Voting System</title>
+    </head>
+    <body>
+        <h1>Online Voting System</h1>
+        <hr/>
+        <form action="">
+            <h3>Login</h3>
+            <br/><br/>
+            <input type="number" name="mobile" placeholder="Enter mobile number"/>
+            <br/><br/>
+            <input type="password" name="password" placeholder="Enter password"/>
+            <br/><br/>
+            <select name="role">
+                <option>Voter</option>
+                <option>Group</option>
+            </select>
+            <br/><br/>
+            <button>Login</button>
+            <br/><br/>
+            New User?<a href="routes/register.html">Register here</a>
+        </form>
+    </body>
+</html>
