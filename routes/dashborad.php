e5f79ac383d4b74779f103d19153df72d76fff44
AyushiAnand70047 <ayushianand088@gmail.com>
2024-09-21 11:57:02 +0530

    dashboard structure

---
+++ routes/dashborad.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Online Voting System - Dashborad</title>
+    </head>
+    <body>
+        <button>Back</button>
+        <button>Logout</button>
+        <h1>Online Voting System</h1>
+        <hr/>
+        <div id="profile"></div>
+        <div id="group"></div>
+    </body>
+</html>
